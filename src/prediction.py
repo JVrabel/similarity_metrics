b204359ba25b6ba641f867915b4efc1d469d91b9
JVrabel <jakub.vrabel@vutbr.cz>
2023-04-17 00:09:16 +0200

    load model and predict IN PROGRESS

--- src/prediction.py
+++ src/prediction.py
@@ -1,9 +1,23 @@
 
+import torch
+import prediction_engine
+import siamese_net
+import numpy as np
+
+NUM_EPOCHS = 50
 BATCH_SIZE = 128
+LEARNING_RATE = 0.0001
+INPUT_SIZE = 40000
+CHANNELS=50
+KERNEL_SIZES=[50, 10]
+STRIDES=[2, 2]
+PADDINGS=[1, 1]
+HIDDEN_SIZES=[256]
+
 # Setup directories
 test_dir = "datasets/contest_TEST.h5"
-test_labels_dir = "datasets/tests_labels.csv"
-model_dir = 'models/my_model.pth'
+test_labels_dir = "datasets/test_labels.csv"
+model_dir = 'models/model_contest_100ep_maxpool.pth'
 
 # Setup target device
 device = torch.device("cpu")
@@ -17,11 +31,12 @@ test_dataloader, y_test = prediction_engine.create_dataloaders(
 )
 
 
-saved_state_dict = torch.load(model_dir)
+saved_state_dict = torch.load(model_dir, map_location=torch.device('cpu'))
+
 # Create a new instance of your model
 model = siamese_net.SiameseNetwork(
     input_size=INPUT_SIZE, 
-    output_size=len(np.unique(train_labels)), 
+    output_size=12, 
     channels=CHANNELS, 
     kernel_sizes=KERNEL_SIZES, 
     strides=STRIDES, 
@@ -42,5 +57,8 @@ prediction_X_test = prediction_engine.predict_test(
                     batch_size=BATCH_SIZE,
                     y_test=y_test
                     )
+
+
+np.save('datasets/prediction_X_test.npy', prediction_X_test)                  
 
 #https://colab.research.google.com/drive/15D5vAYkhbAs5-txhYTCb_Fp2jiCnHXVN#scrollTo=82F_qINOBbkL
